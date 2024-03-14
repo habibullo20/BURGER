b6663ae5779daa35e59e2d5b378bc68e7baa9c7d
habibullo20 <habibulloqudratillayev@gmail.com>
2024-03-14 15:32:21 +0500

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/header/header"
+
 function App() {
   
 
   return (
     <>
-      HELLO
+      <Header/>
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,35 @@
+import "./header.css"
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+        <div className="container">
+            <div className="header-inner">
+                <div className="header-text">
+                    <h2>
+                        BURGER <span>GRILL</span>
+                    </h2>
+
+                    <ul>
+                        <li className="item">
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">About us</a>
+                        </li>
+                        <li className="item">
+                            <a href="">Menu</a>
+                        </li>
+                        <li className="item">
+                            <a href="">Features</a>
+                        </li>
+                        <li className="item">
+                            <a href="">Contact us</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
