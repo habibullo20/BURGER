89ac06e88b8d245e402edbb77e8d560eb7d5f4fd
habibullo20 <habibulloqudratillayev@gmail.com>
2024-03-19 14:16:16 +0500

    Delivery

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
 import { About } from "./components/about/about"
+import Delivery from "./components/delivery/delivery"
 import Features from "./components/features/features"
 import Header from "./components/header/header"
 import Hero from "./components/hero/hero"
+import Menu from "./components/hero/menu/menu"
 
 function App() {
   
@@ -10,11 +11,11 @@ function App() {
   return (
     <>
       <Header/>
-      {/* <Hero/>
+      <Hero/>
       <Features/>
-      <main>
-        <About/>
-      </main> */}
+      <About/>
+      <Menu/>
+      <Delivery/>
     </>
   )
 }
